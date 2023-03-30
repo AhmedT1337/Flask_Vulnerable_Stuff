45c2819015c557b3fd5de2b05a0429fb5c796d3c
Ahmed Tareq <63518121+AhmedT1337@users.noreply.github.com>
2023-03-30 13:38:19 +0200

    Update fixed.py

--- SSTI and XSS/fixed.py
+++ SSTI and XSS/fixed.py
@@ -5,5 +5,21 @@ def user() :
     name = request.args.get("name")
     return render_template("user.html", name=name)
 
+"""
+OR you can use this code :
+from flask import Flask, render_template, request, escape
+
+
+@app.route("/user")
+def user() :
+    name = request.args.get("name")
+    return f"<h1>Hello {escape(name)}</h1>"
+
+
+if __name__ == '__main__':
+    app.run()
+    
+"""
+
 if __name__ == '__main__':
     app.run()
